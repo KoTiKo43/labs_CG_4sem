bb9ee9bc6147da315ec09f22515886c8c2fa1010
KoTiKo43 <akimnik2005@mail.ru>
2025-03-28 01:43:15 +0400

    Глобальный рефакторинг кода

--- Лр4/task17.py
+++ Лр4/task17.py
@@ -2,40 +2,48 @@ import numpy as np
 from PIL import Image, ImageOps
 from math import ceil, floor, sqrt, cos, sin, pi
 
+# Конфигурационные параметры
 H, W = 2000, 2000
-Ht, Wt = 1024,1024
-u0, v0 = W/2, H/2
-ax, ay = 1000, 1000
-img_arr = np.zeros((H, W, 3), dtype=np.uint8)
-img_arr_text = np.zeros((H, W,3), dtype=np.uint8)
+HT, WT = 1024, 1024
+U0, V0 = W/2, H/2
+AX, AY = 1000, 1000
+LIGHT_DIR = np.array([0, 0, 1])
+
+# Инициализация буферов
+img_mat = np.zeros((H, W, 3), dtype=np.uint8)
+img_text = np.zeros((H, W, 3), dtype=np.uint8)
 z_buffer = np.full((H, W), np.inf, dtype=float)
 
+def barycentric_coordinates(x, y, x0, y0, x1, y1, x2, y2):
+    denominator = (x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2)
+    lambda0 = ((x - x2)*(y1 - y2) - (x1 - x2)*(y - y2)) / denominator
+    lambda1 = ((x0 - x2)*(y - y2) - (x - x2)*(y0 - y2)) / denominator
+    return (lambda0, lambda1, 1.0 - lambda0 - lambda1)
 
-def baricentric_coordinates(x, y, x0, y0, x1, y1, x2, y2):
-    lambda0 = ((x - x2) * (y1 - y2) - (x1 - x2) * (y - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
-    lambda1 = ((x0 - x2) * (y - y2) - (x - x2) * (y0 - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
-    lambda2 = 1.0 - lambda0 - lambda1
-    return (lambda0, lambda1, lambda2)
+def compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2):
+    v1 = np.array([x1-x0, y1-y0, z1-z0])
+    v2 = np.array([x2-x0, y2-y0, z2-z0])
+    return np.cross(v1, v2)
 
+def project_vertex(x, y, z):
+    return AX*x/z + U0, AY*y/z + V0
 
-def get_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2):
-    return ((y1 - y2) * (z1 - z0) - (y1 - y0) * (z1 - z2), (z1 - z2) * (x1 - x0) - (x1 - x2) * (z1 - z0),
-            (x1 - x2) * (y1 - y0) - (y1 - y2) * (x1 - x0))
+def norm_scalar(normal):
+    norm = np.linalg.norm(normal)
+    return np.dot(LIGHT_DIR, normal) / norm
 
-
-l = [0, 0, 1]
-def norm_scalar(n):
-    global l
-    return np.dot(l, n) / (sqrt((l[0] ** 2 + l[1] ** 2 + l[2] ** 2)) * sqrt((n[0] ** 2 + n[1] ** 2 + n[2] ** 2)))
-
-
-def draw_triangle(img_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, color1, color2, color3):
-    x0_proj, y0_proj = ax * x0 / z0 + u0, ay * y0 / z0 + v0
-    x1_proj, y1_proj = ax * x1 / z1 + u0, ay * y1 / z1 + v0
-    x2_proj, y2_proj = ax * x2/ z2 + u0, ay * y2 / z2 + v0
-
-    xmin, xmax, ymin, ymax = floor(min(x0_proj, x1_proj, x2_proj)), ceil(max(x0_proj, x1_proj, x2_proj)), floor(min(y0_proj, y1_proj, y2_proj)), ceil(
-        max(y0_proj, y1_proj, y2_proj))
+def draw_triangle(img_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, i0, i1, i2):
+    # Проекция вершин
+    proj0 = project_vertex(x0, y0, z0)
+    proj1 = project_vertex(x1, y1, z1)
+    proj2 = project_vertex(x2, y2, z2)
+    
+    # Ограничивающий прямоугольник
+    xmin = floor(min(proj0[0], proj1[0], proj2[0]))
+    xmax = ceil(max(proj0[0], proj1[0], proj2[0]))
+    ymin = floor(min(proj0[1], proj1[1], proj2[1]))
+    ymax = ceil(max(proj0[1], proj1[1], proj2[1]))
+    
     if (xmin < 0):
         xmin = 0
     if (xmax > W):
@@ -44,30 +52,33 @@ def draw_triangle(img_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, color1, color2, c
         ymin = 0
     if (ymax > H):
         ymax = H
-    if norm_scalar(norm)>0:
-        return
-    else:
-        for i in range(xmin, xmax):
-            for j in range(ymin, ymax):
-                coords = baricentric_coordinates(i, j, x0_proj, y0_proj, x1_proj, y1_proj, x2_proj, y2_proj)
-                z_source = coords[0]*z0 + coords[1]*z1 + coords[2]*z2
-                if coords[0] >= 0 and coords[1] >= 0 and coords[2] >= 0:
-                    if z_source <= z_buffer[j][i]:
-                        intence = color1 * coords[0] + color2 * coords[1] + color3 * coords[2]
-                        img_mat[j][i][0], img_mat[j][i][1], img_mat[j][i][2] = -255*intence, -100*intence, 0
-                        z_buffer[j][i] = z_source
+    
+    # Проверка ориентации
+    normal = compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
+    if norm_scalar(normal) > 0: return
 
-def turn(x, y, z, a, b, g, tx = 0, ty = 0, tz = 0):
-    R = np.array([[1, 0, 0], [0, cos(a), sin(a)], [0, -sin(a), cos(a)]]).dot([[cos(b), 0, sin(b)], [0, 1, 0], [-sin(b), 0, cos(b)]]).dot(np.array([[cos(g), sin(g), 0], [-sin(g), cos(g), 0], [0, 0, 1]]))
-    return R.dot(np.array([x, y, z])) + np.array([tx, ty, tz])
+    # Растеризация
+    for i in range(xmin, xmax):
+        for j in range(ymin, ymax):
+            lambdas = barycentric_coordinates(i, j, *proj0, *proj1, *proj2)
+            if all(l >= 0 for l in lambdas):
+                z = lambdas[0]*z0 + lambdas[1]*z1 + lambdas[2]*z2
+                if z < z_buffer[j][i]:
+                    intensity = sum(l*c for l, c in zip(lambdas, [i0, i1, i2]))
+                    img_mat[j][i][0], img_mat[j][i][1], img_mat[j][i][2] = -255*intensity, -100*intensity, 0
+                    z_buffer[j][i] = z
 
-def draw_texture(img_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, coords_t0, coords_t1, coords_t2, file_texture,i0,i1,i2):
-    x0_proj, y0_proj = ax * x0 / z0 + u0, ay * y0 / z0 + v0
-    x1_proj, y1_proj = ax * x1 / z1 + u0, ay * y1 / z1 + v0
-    x2_proj, y2_proj = ax * x2/ z2 + u0, ay * y2 / z2 + v0
-
-    xmin, xmax, ymin, ymax = floor(min(x0_proj, x1_proj, x2_proj)), ceil(max(x0_proj, x1_proj, x2_proj)), floor(min(y0_proj, y1_proj, y2_proj)), ceil(
-        max(y0_proj, y1_proj, y2_proj))
+def apply_texture(img_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, uv0, uv1, uv2, texture, i0, i1, i2):
+    # Наложение текстуры с учетом освещения
+    proj0 = project_vertex(x0, y0, z0)
+    proj1 = project_vertex(x1, y1, z1)
+    proj2 = project_vertex(x2, y2, z2)
+    
+    xmin = floor(min(proj0[0], proj1[0], proj2[0]))
+    xmax = ceil(max(proj0[0], proj1[0], proj2[0]))
+    ymin = floor(min(proj0[1], proj1[1], proj2[1]))
+    ymax = ceil(max(proj0[1], proj1[1], proj2[1]))
+    
     if (xmin < 0):
         xmin = 0
     if (xmax > W):
@@ -76,78 +87,101 @@ def draw_texture(img_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, coords_t0, coords_
         ymin = 0
     if (ymax > H):
         ymax = H
-    if norm_scalar(norm)>0:
-        return
-    else:
-        for i in range(xmin, xmax):
-            for j in range(ymin, ymax):
-                coords = baricentric_coordinates(i, j, x0_proj, y0_proj, x1_proj, y1_proj, x2_proj, y2_proj)
-                z_source = coords[0]*z0 + coords[1]*z1 + coords[2]*z2
-                if coords[0] >= 0 and coords[1] >= 0 and coords[2] >= 0:
-                    if z_source <= z_buffer[j][i]:
-                        textur_c = [round(Ht*(coords[0]*coords_t0[0]+coords[1]*coords_t1[0]+coords[2]*coords_t2[0])),
-                                    round(Wt*(coords[0]*coords_t0[1]+coords[1]*coords_t1[1]+coords[2]*coords_t2[1]))]
+    
+    normal = compute_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
+    if norm_scalar(normal) > 0: return
+
+    for i in range(xmin, xmax):
+        for j in range(ymin, ymax):
+            lambdas = barycentric_coordinates(i, j, *proj0, *proj1, *proj2)
+            if all(l >= 0 for l in lambdas):
+                z = lambdas[0]*z0 + lambdas[1]*z1 + lambdas[2]*z2
+                if z < z_buffer[j][i]:
+                    textur_c = [round(HT*(lambdas[0]*uv0[0]+lambdas[1]*uv1[0]+lambdas[2]*uv2[0])),
+                                round(WT*(lambdas[0]*uv0[1]+lambdas[1]*uv1[1]+lambdas[2]*uv2[1]))]
                                                                      
-                        color = file_texture.getpixel((textur_c[0],textur_c[1]))
+                    color = texture.getpixel((textur_c[0],textur_c[1]))
 
-                        img_mat[j][i] = (#-color[0]*(i0*coords[0]+i1*coords[1]+i2*coords[2]),
-                                         #-color[1]*(i0*coords[0]+i1*coords[1]+i2*coords[2]),
-                                         #-color[2]*(i0*coords[0]+i1*coords[1]+i2*coords[2]))
-                                         color)
-                        z_buffer[j][i] = z_source
+                    img_mat[j][i] = (#-color[0]*(i0*lambdas[0]+i1*lambdas[1]+i2*lambdas[2]),
+                                    #-color[1]*(i0*lambdas[0]+i1*lambdas[1]+i2*lambdas[2]),
+                                    #-color[2]*(i0*lambdas[0]+i1*lambdas[1]+i2*lambdas[2]))
+                                    color)
+                    z_buffer[j][i] = z
 
-file = open(r'C:\\Users\Nikita\Documents\КГ Лабы\labs_CG_4sem\Лр4\zayac.obj')
-file_texture =Image.open(r'C:\\Users\Nikita\Documents\КГ Лабы\labs_CG_4sem\Лр4\zayac_texture.jpg')
-file_texture=ImageOps.flip(file_texture)
-arr_n = []
-arr = []
-arr_texture_coords = []
-for s in file:
-    spl_s = s.split()
-    if (spl_s[0] == 'v'):
-        arr.append([float(x) for x in spl_s[1:]])
-    if (spl_s[0] == 'vt'):
-        arr_texture_coords.append([float(x) for x in spl_s[1:]])
-    if (spl_s[0] == 'f'):
-        arr_n.append([(int(x.split('/')[0]) - 1, int(x.split('/')[1]) - 1) for x in spl_s[1:]])
+def rotate_vertex(x, y, z, angles, translate):
+    # Поворот и трансляция вершины
+    rx = np.array([[1, 0, 0], 
+                 [0, cos(angles[0]), sin(angles[0])], 
+                 [0, -sin(angles[0]), cos(angles[0])]])
+    
+    ry = np.array([[cos(angles[1]), 0, sin(angles[1])], 
+                 [0, 1, 0], 
+                 [-sin(angles[1]), 0, cos(angles[1])]])
+    
+    rz = np.array([[cos(angles[2]), sin(angles[2]), 0], 
+                 [-sin(angles[2]), cos(angles[2]), 0], 
+                 [0, 0, 1]])
+    
+    rotated = np.dot(rz, np.dot(ry, np.dot(rx, np.array([x, y, z]))))
+    return rotated + np.array(translate)
 
-vn_cal = np.zeros((len(arr),3))
+# Загрузка данных
+model = []
+texture_coords = []
+faces = []
 
-for i in arr:
-    i[0], i[1], i[2] = turn(i[0], i[1], i[2], 0, pi/2, 0, ty = -0.03, tz = 0.01)
+with open(r'C:\\Users\akimn\Documents\Лабы КГ\labs_CG_4sem\Лр4\zayac.obj') as f:
+    for line in f:
+        parts = line.split()
+        if not parts: continue
+        if parts[0] == 'v': model.append(list(map(float, parts[1:4])))
+        elif parts[0] == 'vt': texture_coords.append(list(map(float, parts[1:3])))
+        elif parts[0] == 'f': faces.append([tuple(map(int, v.split('/'))) for v in parts[1:4]])
 
-for i in arr_n:
-    x0, y0, z0 = arr[i[0][0]][0], arr[i[0][0]][1], arr[i[0][0]][2]
-    x1, y1, z1 = arr[i[1][0]][0], arr[i[1][0]][1], arr[i[1][0]][2]
-    x2, y2, z2 = arr[i[2][0]][0], arr[i[2][0]][1], arr[i[2][0]][2]
+# Применение трансформаций
+for i in range(len(model)):
+    model[i] = rotate_vertex(*model[i], # x, y, z
+                             (0, pi/2, 0), # Углы
+                             (0, -0.03, 0.2) # tx, ty, tz
+                             )
 
-    norm = get_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
-    vn_cal[i[0][0]-1]+=norm
-    vn_cal[i[1][0]-1]+=norm
-    vn_cal[i[2][0]-1]+=norm
+# Вычисление вершинных нормалей
+vertex_normals = np.zeros((len(model), 3))
+for face in faces:
+    v0, v1, v2 = [model[idx[0]-1] for idx in face]
+    normal = compute_normal(*v0, *v1, *v2)
+    for idx in face:
+        vertex_normals[idx[0]-1] += normal
 
-for i in range(len(vn_cal)):
-    vn_cal[i] = vn_cal[i]/sqrt(vn_cal[i][0]**2+vn_cal[i][1]**2+vn_cal[i][2]**2)
+# Нормализация нормалей
+vertex_normals = [n/np.linalg.norm(n) if np.linalg.norm(n)!=0 else n for n in vertex_normals]
 
-for i in arr_n:    
-    x0, y0, z0 = arr[i[0][0]][0], arr[i[0][0]][1], arr[i[0][0]][2]
-    x1, y1, z1 = arr[i[1][0]][0], arr[i[1][0]][1], arr[i[1][0]][2]
-    x2, y2, z2 = arr[i[2][0]][0], arr[i[2][0]][1], arr[i[2][0]][2]
+# Основной цикл рендеринга
+texture = ImageOps.flip(Image.open(r'C:\\Users\akimn\Documents\Лабы КГ\labs_CG_4sem\Лр4\zayac_texture.jpg'))
 
-    norm = get_normal(x0, y0, z0, x1, y1, z1, x2, y2, z2)
-    draw_triangle(img_arr, x0, y0, z0, x1, y1, z1, x2, y2, z2, norm_scalar(vn_cal[i[0][0]-1]), norm_scalar(vn_cal[i[1][0]-1]), norm_scalar(vn_cal[i[2][0]-1]))
-    draw_texture(img_arr_text, x0, y0, z0, x1, y1, z1, x2, y2, z2, 
-                 (arr_texture_coords[i[0][1]][0], arr_texture_coords[i[0][1]][1]), 
-                 (arr_texture_coords[i[1][1]][0], arr_texture_coords[i[1][1]][1]), 
-                 (arr_texture_coords[i[2][1]][0], arr_texture_coords[i[2][1]][1]),
-                 file_texture,
-                 norm_scalar(vn_cal[i[0][0]-1]), norm_scalar(vn_cal[i[1][0]-1]), norm_scalar(vn_cal[i[2][0]-1]))
+for face in faces:
+    # Получение данных для треугольника
+    v_indices = [idx[0]-1 for idx in face]
+    vt_indices = [idx[1]-1 for idx in face]
+    
+    # Геометрические данные
+    v0, v1, v2 = [model[i] for i in v_indices]
+    intensities = [norm_scalar(vertex_normals[i]) for i in v_indices] # I0, I1, I2
+    
+    # Данные текстуры
+    uv0 = texture_coords[vt_indices[0]]
+    uv1 = texture_coords[vt_indices[1]]
+    uv2 = texture_coords[vt_indices[2]]
+    
+    # Отрисовка
+#    draw_triangle(img_mat, *v0, *v1, *v2, *intensities)
+    apply_texture(img_text, *v0, *v1, *v2, uv0, uv1, uv2, texture, *intensities)
 
-image = Image.fromarray(img_arr, mode='RGB')
-image2 = Image.fromarray(img_arr_text, mode='RGB')
+image = Image.fromarray(img_mat, mode='RGB')
+image2 = Image.fromarray(img_text, mode='RGB')
 image = ImageOps.flip(image)
 image2 = ImageOps.flip(image2)
 image.show()
 image2.show()
-image.save(r'C:\\Users\Nikita\Documents\КГ Лабы\labs_CG_4sem\ЛР4\Крол.png')
-image2.save(r'C:\\Users\Nikita\Documents\КГ Лабы\labs_CG_4sem\ЛР4\КролТекст.png')
+#image.save(r'C:\\Users\akimn\Documents\Лабы КГ\labs_CG_4sem\ЛР4\Крол.png')
+image2.save(r'C:\\Users\akimn\Documents\Лабы КГ\labs_CG_4sem\ЛР4\КролТекст.png')
